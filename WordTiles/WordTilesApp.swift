a3897dca24f94dc08177cd75c11816ee5da742be
Kim <nordinsane@gmail.com>
2024-11-05 21:16:39 +0100

    Initial Commit

---
+++ WordTiles/WordTilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordTilesApp.swift
+//  WordTiles
+//
+//  Created by Kim on 2024-11-05.
+//
+
+import SwiftUI
+
+@main
+struct WordTilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
