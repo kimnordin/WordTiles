04ee607f4637091c16d3ce2b93e4074db4d7d611
Kim <nordinsane@gmail.com>
2024-11-06 22:58:24 +0100

    Set Inner Tile Selection Area

--- WordTiles/Views/TileView.swift
+++ WordTiles/Views/TileView.swift
@@ -13,27 +13,30 @@ struct TileView: View {
     let gridPosition: CGPoint
 
     var body: some View {
-        ZStack {
-            Rectangle()
-                .fill(isSelected ? .black.opacity(0.3) : .clear)
-                .background(
-                    Text("\(letter.points)").font(.system(size: 10)) .padding(.trailing, 3).minimumScaleFactor(0.1), alignment: .topTrailing
-                )
-            Text(letter.character)
-                .font(.largeTitle)
-                .minimumScaleFactor(0.1)
-        }
-        .background(letter.color)
-        .cornerRadius(8)
-        .contentShape(Rectangle())
-        .overlay(
-            GeometryReader { geometry in
-                Color.clear.preference(
-                    key: TileSelectionPreferenceKey.self,
-                    value: [gridPosition: geometry.frame(in: .named("GameView"))]
-                )
+        GeometryReader { outerGeometry in
+            ZStack {
+                Rectangle()
+                    .fill(isSelected ? .black.opacity(0.3) : .clear)
+                    .background(
+                        Text("\(letter.points)").font(.system(size: 10)) .padding(.trailing, 3).minimumScaleFactor(0.1), alignment: .topTrailing
+                    )
+                Text(letter.character)
+                    .font(.largeTitle)
+                    .minimumScaleFactor(0.1)
             }
-        )
+            .background(letter.color)
+            .cornerRadius(8)
+            .contentShape(Rectangle())
+            .overlay(
+                GeometryReader { innerGeometry in
+                    Color.clear.preference(
+                        key: TileSelectionPreferenceKey.self,
+                        value: [gridPosition: innerGeometry.frame(in: .named("GameView"))]
+                    )
+                }
+                .padding(outerGeometry.size.width / 8)
+            )
+        }
     }
 }
 
